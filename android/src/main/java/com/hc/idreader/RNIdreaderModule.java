223ed1a1fdd36b11d95a82051655f2bb6833f7b3
JianLong Nie <>
2020-07-16 13:45:15 +0800

    适配最新版react androidx

--- android/src/main/java/com/hc/idreader/RNIdreaderModule.java
+++ android/src/main/java/com/hc/idreader/RNIdreaderModule.java
@@ -9,7 +9,7 @@ import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 import android.widget.Toast;
 
